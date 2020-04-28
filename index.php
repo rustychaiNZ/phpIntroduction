900e4fe80b714a15da28b22c089a485c1f13e9e2
rustychaiNZ <rusljohn@gmail.com>
2020-04-28 16:37:38 +1200

    talked about echo, variables, comments and scopes

--- index.php
+++ index.php
@@ -38,14 +38,17 @@
 				</div>
 			</div>
 			<div class="col-sm-6">
-				<img src="./images/phpImage" alt="PHP logo image" class="img-fluid">
+				<img src="./images/phpImage.jpeg" alt="PHP logo image" class="img-fluid py-3">
 			</div>
 		</div>
 		<div class="row d-flex bg-dark">
-			<div class="col-6 align-self-center">
-				<h2 class="display-1 text-light">PHP Examples</h2>
+			<!-- Section header -->
+			<div class="col-lg-4 align-self-center">
+				<h2 class="display-3 text-light text-center">PHP Examples <br>Day 1</h2>
 			</div>
-			<div class="col-6 bg-white">
+
+			<!-- PHP contents -->
+			<div class="col-lg-8 bg-white">
 				<!-- Echo -->
 				<h2 class="section">PHP echo</h2>
 				<?php // PHP echo start
@@ -65,13 +68,56 @@
 
 				<!-- Variables -->
 				<h2 class="section">Variables</h2>
-				<p>Similar to JS, PHP also has variables that can be either static or dynamically changed. Variables are represented by <span class="highlighted">$</span> followed by the name of the variable. For example</p>
-				<?php
-					$hello = "Hello world. This is a message stored as a variable called $hello in php and then dynamically added to the page through an echo call.";
+				<p>Similar to JS, PHP also has variables that can be either static or dynamically changed. Variables are represented by <span class="highlighted">$</span> followed by the name of the variable. If you want to referencec a variable without it displaying the contents, you must use an escape function using '\' in front of the '$'. Another important point is that unlike keywords, variables are case sensitive. For example:</p>
+				<?php // Intro to variables
+					$hello = "Hello world. This is a message stored as a variable called '<b>\$hello</b>' in php and then dynamically added to the page through an echo call.";
 
-					echo "$hello";
+					echo $hello . "<br>";
+
+					$number1 = 1.99;
+					$number2 = 5;
+
+					echo "Here is an example of numbers and floats being used together: number1 = " . $number1 . " and number2 = " . $number2 . " and the sum of these two are = " . ($number1 + $number2);
+				?> <!-- Variables end -->
+
+				<!-- Scopes -->
+				<h2 class="section">Variable Scopes</h2>
+				<p>PHP has 3 main ways in which variables are able to be declared anywhere in the script. The scope refers to where the variable has been both declared and where it can be used.</p>
+				<p>PHP has three different variable scopes:</p>
+				<ul>
+					<li><b>Global:</b> A variable decleared <b>outside</b> a function and can only be accessed outside a function.</li>
+					<li><b>Local:</b> A variable that is decleared <b>within</b> a function and can only be accessed within said function.</li>
+					<li><b>Static:</b> A variable that isn't deleted after function has been run and is avalible for a further job.</li>
+				</ul>
+				<h3>Examples:</h3>
+				<h4 class="text-secondary">Global</h4>
+				<?php // Global variables example
+					$globalVar = "This is a global variable and can be used anywhere outside of a function.";
+					echo "This is using a global variable: <b>" . $globalVar . "</b>";
 				?>
-
+				<h4 class="text-secondary">Local</h4>
+				<?php // Local variable example
+					function localFunction(){
+						$localVar = "This is a local variable and can only be used inside of a function that it has been defined in.";
+						echo "This is using a local variable: <b>" . $localVar . "</b>";
+					}
+					localFunction();
+				?>
+				<h4 class="text-secondary">Static</h4>
+				<?php // Static variable example
+					function staticFunction(){
+						static $x = 99;
+						static $it = 0;
+						echo $x . " is iteration " . $it . "<br>";
+						$x++;
+						$it++;
+					}
+					staticFunction();
+					staticFunction();
+					staticFunction();
+					staticFunction();
+				?>
+				<!-- Scopes end -->
 			</div>
 		</div>
 	</div>
